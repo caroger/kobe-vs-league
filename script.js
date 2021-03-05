b0ce26b6faaa97dd11da91801014462ebfc547ac
caroger <rogerhu1989@gmail.com>
2021-03-05 09:59:04 -0800

    map arenas to us map

--- script.js
+++ script.js
@@ -25,4 +25,23 @@ d3.json("/data/us.json").then((data) => {
     .attr("fill", "#fdb927") // base state color
     .attr("stroke", "white") // white state border
     .attr("stroke-width", 5); // state line width
+
+  //map team logo
+  d3.json("/data/arenas.geojson").then((data) => {
+    // console.log(topojson.feature(data, data));
+    console.log(data.features);
+    svg
+      .selectAll("circle")
+      .data(data.features)
+      .enter()
+      .append("circle")
+      .attr("cx", function (d) {
+        return projection(d.geometry.coordinates)[0];
+      })
+      .attr("cy", function (d) {
+        return projection(d.geometry.coordinates)[1];
+      })
+      .attr("r", "8px")
+      .attr("fill", "red");
+  });
 });
