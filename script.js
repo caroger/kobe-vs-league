fdecdf8d15246a0c82e3b96066afa2409bb592c3
caroger <rogerhu1989@gmail.com>
2021-03-06 10:51:07 -0800

    logo to map

--- script.js
+++ script.js
@@ -4,11 +4,14 @@ const svg = d3
   .select(".visual")
   .append("svg")
   .attr("class", "map")
-  .attr("width", width)
-  .attr("height", height);
+  // .attr("width", width)
+  // .attr("height", height);
+  .attr("preserveAspectRatio", "xMinYMin meet")
+  .attr("viewBox", "0 0 960 500");
 
 const projection = d3.geoAlbersUsa();
 const path = d3.geoPath(projection);
+var tooltip = d3.select("image").append("div").attr("class", "hidden tooltip");
 
 d3.json("/data/us.json").then((data) => {
   svg
@@ -29,19 +32,36 @@ d3.json("/data/us.json").then((data) => {
   //map team logo
   d3.json("/data/arenas.geojson").then((data) => {
     // console.log(topojson.feature(data, data));
+    let mouseOver = function (d) {
+      d3.selectAll("image").transition().duration(200).style("opacity", 0.5);
+      d3.select(this)
+        .transition()
+        .duration(200)
+        .style("opacity", 1)
+        .style("stroke", "black");
+    };
+
+    let mouseLeave = function (d) {
+      d3.selectAll("image").transition().duration(200).style("opacity", 0.8);
+      d3.select(this).transition().duration(200).style("stroke", "transparent");
+    };
+
     console.log(data.features);
     svg
-      .selectAll("circle")
+      .selectAll("logo")
       .data(data.features)
       .enter()
-      .append("circle")
-      .attr("cx", function (d) {
-        return projection(d.geometry.coordinates)[0];
+      .append("svg:image")
+      .attr("width", width / 10)
+      .attr("height", height / 6.67)
+      .attr("x", function (d) {
+        return projection(d.geometry.coordinates)[0] - width / 10 / 2;
       })
-      .attr("cy", function (d) {
-        return projection(d.geometry.coordinates)[1];
+      .attr("y", function (d) {
+        return projection(d.geometry.coordinates)[1] - height / 6.67 / 2;
       })
-      .attr("r", "8px")
-      .attr("fill", "red");
+      .attr("xlink:href", (d) => d.properties.logo_url)
+      .on("mouseover", mouseOver)
+      .on("mouseleave", mouseLeave);
   });
 });
