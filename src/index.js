9354006ad6e7ab9e3df930513be9d932e317d468
caroger <rogerhu1989@gmail.com>
2021-03-08 01:47:52 -0800

    combine gameData with arenaData

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
-import { select, geoAlbersUsa, geoPath, json, selectAll } from "d3";
-import { topojson } from "topojson";
+import { select, geoAlbersUsa, geoPath, json, selectAll, csv } from "d3";
 
 const width = 900;
 const height = 600;
@@ -11,13 +10,40 @@ const svg = select(".visual")
 
 const projection = geoAlbersUsa();
 const path = geoPath(projection);
-var tooltip = select("image").append("div").attr("class", "hidden tooltip");
 
-json("./assets/data/us.json").then((data) => {
+// Helper functions with mouseOver effects
+const mouseOver = function (d) {
+  selectAll("image").transition().duration(100).style("opacity", 0.5);
+  select(this)
+    .transition()
+    .duration(200)
+    .style("opacity", 1)
+    .style("stroke", "black");
+};
+
+const mouseLeave = function (d) {
+  selectAll("image").transition().duration(100).style("opacity", 0.8);
+  select(this).transition().duration(100).style("stroke", "transparent");
+};
+
+Promise.all([
+  json("./assets/data/gz_2010_us_040_00_20m.json"),
+  json("./assets/data/arenas.geojson"),
+  csv("./assets/data/game_stats.csv"),
+]).then(([mapData, arenaData, gameData]) => {
+  const gamesByOpponents = gameData.reduce((r, a) => {
+    r[a.OPPONENTS] = r[a.OPPONENTS] || [];
+    r[a.OPPONENTS].push(a);
+    return r;
+  }, Object.create(null));
+
+  console.log(gameData);
+  console.log(gamesByOpponents["ATL"]);
+
   svg
     .selectAll("path")
     .data(
-      data.features.filter(
+      mapData.features.filter(
         (d) => !["Alaska", "Hawaii"].includes(d.properties.NAME)
       )
     )
@@ -29,37 +55,20 @@ json("./assets/data/us.json").then((data) => {
     .attr("stroke", "white") // white state border
     .attr("stroke-width", 5); // state line width
 
-  //map team logo
-  json("./assets/data/arenas.geojson").then((data) => {
-    let mouseOver = function (d) {
-      selectAll("image").transition().duration(200).style("opacity", 0.5);
-      select(this)
-        .transition()
-        .duration(200)
-        .style("opacity", 1)
-        .style("stroke", "black");
-    };
-
-    let mouseLeave = function (d) {
-      selectAll("image").transition().duration(200).style("opacity", 0.8);
-      select(this).transition().duration(200).style("stroke", "transparent");
-    };
-
-    svg
-      .selectAll("logo")
-      .data(data.features)
-      .enter()
-      .append("svg:image")
-      .attr("width", width / 10)
-      .attr("height", height / 6.67)
-      .attr("x", function (d) {
-        return projection(d.geometry.coordinates)[0] - width / 10 / 2;
-      })
-      .attr("y", function (d) {
-        return projection(d.geometry.coordinates)[1] - height / 6.67 / 2;
-      })
-      .attr("xlink:href", (d) => d.properties.logo_url)
-      .on("mouseover", mouseOver)
-      .on("mouseleave", mouseLeave);
-  });
+  svg
+    .selectAll("logo")
+    .data(arenaData.features)
+    .enter()
+    .append("svg:image")
+    .attr("width", width / 10)
+    .attr("height", height / 6.67)
+    .attr("x", function (d) {
+      return projection(d.geometry.coordinates)[0] - width / 10 / 2;
+    })
+    .attr("y", function (d) {
+      return projection(d.geometry.coordinates)[1] - height / 6.67 / 2;
+    })
+    .attr("xlink:href", (d) => d.properties.logo_url)
+    .on("mouseover", mouseOver)
+    .on("mouseleave", mouseLeave);
 });
